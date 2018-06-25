6d9a57081cae4d473469a74351dd155e83ee1067
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2018-06-25 13:30:08 -0500

    wip - fix schedule editor

--- js/admin/index.js
+++ js/admin/index.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route } from 'react-router';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
+import {Route} from 'react-router';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'react-router-redux';
 
 import App from './app';
+import ErrorBoundary from '../public/errorBoundary';
 import DevTools from '../devtools';
 import ajaxSetup from '../public/ajaxsetup';
 
 if (__DEVTOOLS__) {
-    window.store = App.store;
+  window.store = App.store;
 }
 
-window.AdminApp = function(props) {
-    ajaxSetup();
-    ReactDOM.render(
-        <Provider store={App.store}>
+window.AdminApp = function (props) {
+  ajaxSetup();
+  ReactDOM.render(
+    <ErrorBoundary>
+      <Provider store={App.store}>
+        <React.Fragment>
+          <ConnectedRouter history={App.history}>
             <React.Fragment>
-                <ConnectedRouter history={App.history}>
-                    <React.Fragment>
-                        <Route path={window.ROOT_PATH} component={App} />
-                    </React.Fragment>
-                </ConnectedRouter>
-                { __DEVTOOLS__ ? <DevTools /> : null}
+              <Route path={window.ROOT_PATH} component={App}/>
             </React.Fragment>
-        </Provider>,
-        document.getElementById("container"));
+          </ConnectedRouter>
+          {__DEVTOOLS__ && false ? <DevTools/> : null}
+        </React.Fragment>
+      </Provider>
+    </ErrorBoundary>,
+    document.getElementById("container"));
 };
